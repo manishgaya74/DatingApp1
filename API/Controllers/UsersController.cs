b82d9f00591de57a905933d37eb3c5e645746a60
Manish Sinha <manishsinhalearning@outlook.com>
2021-05-03 19:14:46 +0530

    Added table, user class and dbcontext

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //https://localhost:5001/api/users   
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
